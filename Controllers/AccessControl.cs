566ea4c1d37083c17fe8a8320ecbd001c7cfd8a1
Nicolas Chourot <45172002+Nicolas-Chourot@users.noreply.github.com>
2025-04-07 09:38:59 -0400

    Little Fix on namespace

--- Controllers/AccessControl.cs
+++ Controllers/AccessControl.cs
@@ -1,12 +1,11 @@
 ﻿using JsonDemo.Models;
-using PhotosManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PhotosManager.Controllers
+namespace JsonDemo.Controllers
 {
     public class AccessControl
     {
